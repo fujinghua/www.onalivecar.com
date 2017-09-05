eef47b9d73172f86643a75a5edf89dd66a1c2263
Phper Helper <909387235@qq.com>
2017-09-05 10:40:54 +0800

    接口模板

--- application/api/controller/HomeController.php
+++ application/api/controller/HomeController.php
@@ -4,6 +4,8 @@ namespace app\api\controller;
 
 use app\common\controller\ApiController;
 
+use app\common\model\Slider;
+
 /**
  * 默认控制器
  * @author Sir Fu
@@ -22,6 +24,27 @@ class HomeController extends ApiController
             'banner' => []
         ];
         return json(['status' => '1', 'info' => 'API']);
+    }
+
+    /**
+     * 首页轮播
+     * @author Sir Fu
+     */
+    public function sliderAction()
+    {
+        $ret = ['status' => '1', 'info' => '成功','data'=>[]];
+        $data = [];
+        $where = ['is_delete'=>'1','status'=>'2'];
+        $field = ['url', 'url_icon', 'target', 'title', 'description',];
+        $limit = 6;
+        $model = Slider::load();
+        $result = $model->where($where)->limit($limit)->order('order ASC')->getField(implode(',',$field));
+        $result = $model->asArray($result); //此方法是我自己写的
+        if (!empty($result)){
+            $data = $result;
+        }
+        $ret['data'] = $data;
+        return json($ret);
     }
 
 
